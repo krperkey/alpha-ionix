62d06f2a6902d2b66df8ff07ee5fed9235d79f52
krperkey <krperkey@gmail.com>
2025-02-19 21:08:11 -0500

    mobile test

--- scripts/mobile.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('nav-toggle').addEventListener('click', function() {
-    document.getElementById('main-nav').classList.toggle('active');
-});
-
-document.getElementById('toolbar-toggle').addEventListener('click', function() {
-    document.querySelector('#toolbar ul').classList.toggle('hidden');
-});

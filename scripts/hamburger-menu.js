38fcfc1c79de80d2b93b87f48ea4e56bb046bdec
krperkey <krperkey@gmail.com>
2025-02-21 16:34:03 -0500

    nav

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburgerBtn = document.getElementById("hamburger-btn");
     const navMenu = document.getElementById("main-nav");
-    const toolbarMenu = document.getElementById("toolbar");
 
     // Toggle the navigation and toolbar visibility when clicking the hamburger menu
     hamburgerBtn.addEventListener("click", function () {
         navMenu.classList.toggle("active");
-        toolbarMenu.classList.toggle("active");
     });
 });
